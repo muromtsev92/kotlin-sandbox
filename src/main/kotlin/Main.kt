24276e0cc5a0cb53a7f640792a6c5a21169a211b
SergeiMuromtsev <k29393@yandex.ru>
2024-02-14 01:19:03 +0500

    phrases

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,17 @@
 fun main(args: Array<String>) {
-    var x: Short = 1
+    val words1 = arrayOf("24/7", "multi-tier", "B-to-B", "dynamic", "pervasive")
+    val words2 = arrayOf("empowered", "leveraged", "aligned", "targeted")
+    val words3 = arrayOf("process", "paradigm", "solution", "portal", "vision")
 
-    while (x.toInt() < 3){
-        print(if (x.toInt() == 1) "Yab" else "Dab" )
-        print("ba")
-        x++
-    }
-    if (x.toInt()==3) println("Do")
+    val arrSize1 = words1.size
+    val arrSize2 = words2.size
+    val arrSize3 = words3.size
+
+    val index1 = (Math.random() * arrSize1).toInt()
+    val index2 = (Math.random() * arrSize2).toInt()
+    val index3 = (Math.random() * arrSize3).toInt()
+
+    val phrase = "${words1[index1]} ${words2[index2]} ${words3[index3]}"
+
+    println(phrase)
 }
