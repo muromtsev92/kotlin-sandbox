f4dac99e244c3acbf34261fff55e036b44678dfe
SergeiMuromtsev <k29393@yandex.ru>
2024-02-14 23:59:42 +0500

    resolving

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,3 @@
 fun main(args: Array<String>) {
-    val song1 = Song("The Mesopotamians", "They might be giants")
-    val song2 = Song("Going Underground", "The Jam")
-    val song3 = Song("Make me smile", "Steve Harvey")
 
-    println(song2.play())
-    println(song2.stop())
-    println(song3.play())
 }
