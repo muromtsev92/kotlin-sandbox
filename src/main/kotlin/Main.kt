cdeb688ebf45f2355135ca796b8cb75508b630d4
SergeiMuromtsev <k29393@yandex.ru>
2024-02-14 23:27:32 +0500

    songs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,9 @@
 fun main(args: Array<String>) {
-    val words1 = arrayOf("24/7", "multi-tier", "B-to-B", "dynamic", "pervasive")
-    val words2 = arrayOf("empowered", "leveraged", "aligned", "targeted")
-    val words3 = arrayOf("process", "paradigm", "solution", "portal", "vision")
+    val song1 = Song("The Mesopotamians", "They might be giants")
+    val song2 = Song("Going Underground", "The Jam")
+    val song3 = Song("Make me smile", "Steve Harvey")
 
-    val arrSize1 = words1.size
-    val arrSize2 = words2.size
-    val arrSize3 = words3.size
-
-    val index1 = (Math.random() * arrSize1).toInt()
-    val index2 = (Math.random() * arrSize2).toInt()
-    val index3 = (Math.random() * arrSize3).toInt()
-
-    val phrase = "${words1[index1]} ${words2[index2]} ${words3[index3]}"
-
-    println(phrase)
+    println(song2.play())
+    println(song2.stop())
+    println(song3.play())
 }
