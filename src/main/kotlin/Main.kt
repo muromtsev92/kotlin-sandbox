f247f5f6a2598a61f1e367db686c007e6b1fef3f
SergeiMuromtsev <k29393@yandex.ru>
2024-02-14 15:16:43 +0500

    minigame-rps

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,60 @@
+
 fun main(args: Array<String>) {
-    val words1 = arrayOf("24/7", "multi-tier", "B-to-B", "dynamic", "pervasive")
-    val words2 = arrayOf("empowered", "leveraged", "aligned", "targeted")
-    val words3 = arrayOf("process", "paradigm", "solution", "portal", "vision")
+    val options = arrayOf("Rock", "Paper", "Scissors")
 
-    val arrSize1 = words1.size
-    val arrSize2 = words2.size
-    val arrSize3 = words3.size
-
-    val index1 = (Math.random() * arrSize1).toInt()
-    val index2 = (Math.random() * arrSize2).toInt()
-    val index3 = (Math.random() * arrSize3).toInt()
-
-    val phrase = "${words1[index1]} ${words2[index2]} ${words3[index3]}"
-
-    println(phrase)
+    while(true){
+        var gameScore = 0
+        var userScore = 0
+        val gameChoice = getGameChoice(options)
+        val userChoice = getUserChoice(options)
+        val winner = getWinner(gameChoice, userChoice)
+        if(winner == "Player") userScore++ else if(winner == "CPU") gameScore++
+        printSummary(winner, gameScore, userScore)
+    }
 }
+
+//    fun getGameChoice(optionsParam: Array<String>): String {
+//        return optionsParam[(Math.random() * optionsParam.size).toInt()]
+//    }
+
+fun getGameChoice(optionsParam: Array<String>) = optionsParam[(Math.random() * optionsParam.size).toInt()]
+
+fun getUserChoice(optionsParam: Array<String>): String {
+
+    var isValidChoice = false
+    var userChoice = ""
+    while(!isValidChoice){
+        print("Make a choice of:")
+        for (x in optionsParam) print(" $x")
+        println(".")
+        val userInput = readlnOrNull()
+        if(userInput != null && userInput in optionsParam){
+            isValidChoice = true
+            userChoice = userInput
+        }
+        if(!isValidChoice) println("Your choice is not valid")
+    }
+    return userChoice
+}
+
+fun getWinner(gameChoice: String, userChoice: String): String {
+    println("CPU choice is $gameChoice || Your choice is $userChoice")
+    if(userChoice == gameChoice) return "Tie!"
+    else if ((userChoice == "Rock" && gameChoice == "Scissors") ||
+        (userChoice == "Scissors" && gameChoice == "Paper") ||
+        (userChoice == "Paper" && gameChoice == "Rock")) {
+
+        return "Player"}
+    else return "CPU"
+}
+
+fun printSummary(winner:String, gameScore: Int, userScore: Int){
+    if(winner == "Tie!") println(winner)
+    else println("Winner is $winner")
+    println("CPU: $gameScore - YOU: $userScore")
+    println("__________________________")
+    println("")
+}
+
+
+
