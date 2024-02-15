c13223ebcd096778d2c869843e8345be769e1801
SergeiMuromtsev <k29393@yandex.ru>
2024-02-15 22:45:26 +0500

    abstract classes

--- src/main/kotlin/animals/Animal.kt
+++ src/main/kotlin/animals/Animal.kt
@@ -1,18 +1,14 @@
 package animals
 
-open class Animal {
-    open val image = ""
-    open val food = ""
-    open val habitat = ""
+abstract class Animal {
+    abstract val image: String
+    abstract val food: String
+    abstract val habitat: String
     var hunger = 10
 
-    open fun makeNoise(){
-        println("the animal is making noise")
-    }
+    abstract fun makeNoise()
 
-    open fun eat(){
-        println("the animal is eating")
-    }
+    abstract fun eat()
 
     open fun roam() {
         println("The animal is roaming")
--- src/main/kotlin/animals/Canine.kt
+++ src/main/kotlin/animals/Canine.kt
@@ -1,7 +1,7 @@
 package animals
 
-open class Canine: Animal() {
-    override fun roam(){
+abstract class Canine: Animal() {
+    override fun roam() {
         println("The canine is roaming")
     }
 }
