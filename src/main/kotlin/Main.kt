1c0c4920d190e9dee44b7a0354cac7ca0ab062a4
SergeiMuromtsev <k29393@yandex.ru>
2024-02-17 21:39:59 +0500

    leet

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,4 +14,16 @@ fun main(args: Array<String>) {
     val s1 = Song("null", "agytin")
     val s2 = Song("null", "agytin")
     println(s1 == s2)
+    var arr = intArrayOf(1,2,1)
+    println(longestCommonPrefix(arrayOf("flower","flight")))
+    println(longestCommonPrefix(arrayOf("ab","a")))
+
+}
+
+fun longestCommonPrefix(strs: Array<String>): String {
+    var current = strs[0]
+    for (i in 0..strs.size-2) {
+        current = current.commonPrefixWith(strs[i+1])
+    }
+    return current
 }
