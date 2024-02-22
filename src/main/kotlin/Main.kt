0ae5680dc9ff6235fc80f4162393719c05d538ba
SergeiMuromtsev <k29393@yandex.ru>
2024-02-22 22:30:32 +0500

    collections pt1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,33 @@
+import collections.Grocery
 
 fun main(args: Array<String>) {
-    var addFive = {x: Int -> x+5}
-    println("Add five to 11 is ${addFive(11)}")
+//    var addFive = {x: Int -> x+5}
+//    println("Add five to 11 is ${addFive(11)}")
+//
+//    val sumInt = {x: Int, y: Int -> x + y}
+//    println("Sum of 5 and 6 is ${sumInt(5,6)}")
+//
+//    convert(5.0) { it * 1.8 + 32 }
+//    convertFive { it * 1.8 + 32 }
 
-    val sumInt = {x: Int, y: Int -> x + y}
-    println("Sum of 5 and 6 is ${sumInt(5,6)}")
+    val groceries = listOf(
+        Grocery("Tomatoes", "Vegetable", "lb", 3.0, 3),
+        Grocery("Mushrooms", "Vegetable", "lb", 4.0, 1),
+        Grocery("Bagels", "Bakery", "Pack", 1.5, 2),
+        Grocery("Olive oil", "Pantry", "Bottle", 6.0, 1),
+        Grocery("Ice cream", "Frozen", "Pack", 3.0, 2)
+    )
 
-    convert(5.0) { it * 1.8 + 32 }
-    convertFive { it * 1.8 + 32 }
+    println(groceries.filter { it.category == "Vegetable" })
+
+
 
 }
 
-fun convert(x: Double,
-            converter: (Double) -> Double): Double{
+fun convert(
+    x: Double,
+    converter: (Double) -> Double
+): Double {
     val result = converter(x)
     println("$x converted to $result")
     return result
---
+++ src/main/kotlin/collections/Grocery.kt
@@ -0,0 +1,5 @@
+package collections
+
+data class Grocery(val name: String, val category: String,
+                   val unit: String, val unitPrice: Double,
+                   val quantity: Int)
